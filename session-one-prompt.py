ba6edd042ec4bc54afbfa225c43707d741814c42
TheBoomOtto2005 <toetting@ucsc.edu>
2026-01-12 20:16:19 -0800

    Add files via upload

---
+++ session-one-prompt.py
@@ -0,0 +1,4 @@
+print("Trace William Oettinger")
+print("He Him His")
+print("My favorite movie is for sure Jurassic Park. I was super into dinosaurs as a kid and just adored jurassic park")
+print("My favorite food is definetely any type of pasta. Ravioli will always be my number 1 though.")
---
+++ session-two-prompt.py
@@ -0,0 +1,21 @@
+x = 12.5
+y = 16.8
+z = x + y
+
+a = 9
+b = 10
+c = b - a
+
+jerry = 15.7
+tom = 6
+cheese = jerry * tom
+
+print("1: 12.5 + 16.8")
+print("2: 10 - 9")
+print("3: 15.7 * 6")
+print(" ")
+print("1: ",z, ",Type ", type(z))
+print("2: ",c, ",Type ", type(c))
+print("3: ",cheese, ",Type ", type(cheese))
+
+
